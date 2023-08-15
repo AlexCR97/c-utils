2d01bc64a21588b89bbadfc5015365e8d68e6287
viex <carp_97@outlook.com>
2023-08-15 11:52:55 -0600

    test(strings): add test for pm_str_split

--- pim/test_strings.c
+++ pim/test_strings.c
@@ -16,9 +16,20 @@ void _test_pm_str_equals() {
 	PM_ASSERT_EQ(are_equal_cd, false);
 }
 
+void _test_pm_str_split() {
+	char* str = "this is a string\0";
+	char** tokens = pm_str_split(str, ' ');
+	PM_ASSERT_EQ_STR(tokens[0], "this");
+	PM_ASSERT_EQ_STR(tokens[1], "is");
+	PM_ASSERT_EQ_STR(tokens[2], "a");
+	PM_ASSERT_EQ_STR(tokens[3], "string");
+	free(tokens);
+}
+
 void test_strings() {
 	Test tests[] = {
 		test(PM_NAMEOF(pm_str_equals), _test_pm_str_equals),
+		test(PM_NAMEOF(pm_str_split), _test_pm_str_split),
 	};
 
 	TestSuite suite = test_suite("pim/strings", tests);
