0d1a7151df744c2242b9932fd22260300fbcf758
viex <carp_97@outlook.com>
2023-08-14 19:40:07 -0600

    refactor(arrays): organize files in sections

--- pim/pim/arrays.c
+++ pim/pim/arrays.c
@@ -6,6 +6,8 @@
 #include "str_string.h"
 #include "str_string_builder.h"
 
+#pragma region int
+
 const int _ARR_DEFAULT_INT = 0;
 
 int* pm_arr_alloc_int(size_t length) {
@@ -147,24 +149,6 @@ int pm_arr_find_index_int(int arr[], size_t arr_length, PmFindFunctionInt predic
     return _arr_find_index_int(arr, arr_length, predicate);
 }
 
-void* pm_arr_map(void* arr, size_t item_size, size_t arr_length, PmMapFunction func) {
-    void* mapped_arr = malloc(item_size * arr_length);  // Allocate memory for the result array
-
-    if (mapped_arr == NULL) {
-        // TODO How to handle error?
-        return NULL;
-    }
-
-    for (size_t i = 0; i < arr_length; i++) {
-        void* current_item = (char*)arr + i * item_size;
-        void* mapped_item = func(i, current_item);
-        memcpy((char*)mapped_arr + i * item_size, mapped_item, item_size); // Copy the modified item to the result array
-        free(mapped_item);  // Free the memory allocated for the modified item
-    }
-
-    return mapped_arr;
-}
-
 int* pm_arr_map_int(int arr[], size_t arr_length, PmMapFunctionInt func) {
     int* mapped_arr = (int*)malloc(sizeof(int) * arr_length);
 
@@ -289,22 +273,7 @@ int* pm_arr_take_int(int arr[], size_t arr_length, size_t count) {
     return result;
 }
 
-char* pm_arr_tail_str(char** arr) {
-    if (arr == NULL || *arr == NULL) {
-        return NULL;  // Return NULL if the input is invalid or the array is empty
-    }
 
-    // Traverse the array to find the last non-null element
-    char** current = arr;
-    while (*current != NULL) {
-        current++;
-    }
-
-    // The pointer at 'current' points to the first NULL element, so we decrement it to get the last non-null element
-    current--;
-
-    return *current;
-}
 
 char* pm_arr_to_string(int* arr, size_t size) {
     StringBuilder sb = string_builder();
@@ -325,3 +294,48 @@ char* pm_arr_to_string(int* arr, size_t size) {
 
     return string_builder_to_string(sb);
 }
+
+#pragma endregion
+
+#pragma region char*
+
+char* pm_arr_tail_str(char** arr) {
+    if (arr == NULL || *arr == NULL) {
+        return NULL;  // Return NULL if the input is invalid or the array is empty
+    }
+
+    // Traverse the array to find the last non-null element
+    char** current = arr;
+    while (*current != NULL) {
+        current++;
+    }
+
+    // The pointer at 'current' points to the first NULL element, so we decrement it to get the last non-null element
+    current--;
+
+    return *current;
+}
+
+#pragma endregion
+
+#pragma region void*
+
+void* pm_arr_map(void* arr, size_t item_size, size_t arr_length, PmMapFunction func) {
+    void* mapped_arr = malloc(item_size * arr_length);  // Allocate memory for the result array
+
+    if (mapped_arr == NULL) {
+        // TODO How to handle error?
+        return NULL;
+    }
+
+    for (size_t i = 0; i < arr_length; i++) {
+        void* current_item = (char*)arr + i * item_size;
+        void* mapped_item = func(i, current_item);
+        memcpy((char*)mapped_arr + i * item_size, mapped_item, item_size); // Copy the modified item to the result array
+        free(mapped_item);  // Free the memory allocated for the modified item
+    }
+
+    return mapped_arr;
+}
+
+#pragma endregion
--- pim/pim/arrays.h
+++ pim/pim/arrays.h
@@ -8,11 +8,28 @@
 #define PM_ARR_DELIMETER_HEAD "{"
 #define PM_ARR_DELIMETER_TAIL "}"
 
-// Allocates an array of int of the specified length
-int* pm_arr_alloc_int(size_t length);
+#pragma region int
 
 // A function that receives the current index and item, and returns a bool
 typedef bool (*PmAnyFunctionInt)(size_t index, int item);
+
+// A function that receives the current index and item, and returns a bool
+typedef bool (*PmCountFunctionInt)(size_t index, int item);
+
+// A function that receives the current index and item, and returns a bool
+typedef bool (*PmEveryFunctionInt)(size_t index, int item);
+
+// A function that receives the current index and item, and returns a bool
+typedef bool (*PmFilterFunctionInt)(size_t index, int item);
+
+// A function that receives the current index and item, and returns a bool
+typedef bool (*PmFindFunctionInt)(size_t index, int item);
+
+// A function that receives the current index and item, and returns a new value
+typedef void* (*PmMapFunctionInt)(size_t index, int item); // TODO Change void* to int?
+
+// Allocates an array of int of the specified length
+int* pm_arr_alloc_int(size_t length);
 
 // Returns true if any of the items in the array satisfies the predicate
 bool pm_arr_any_int(int arr[], size_t arr_length, PmAnyFunctionInt predicate);
@@ -23,41 +40,20 @@ int* pm_arr_concat_int(int arr_a[], size_t arr_a_length, int arr_b[], size_t arr
 // Returns true if the array contains the specified item, false otherwise
 bool pm_arr_contains_int(int arr[], size_t arr_length, int item);
 
-// A function that receives the current index and item, and returns a bool
-typedef bool (*PmCountFunctionInt)(size_t index, int item);
-
 // Returns the amount of items that satisfy the predicate
 size_t pm_arr_count_int(int arr[], size_t arr_length, PmCountFunctionInt predicate);
-
-// A function that receives the current index and item, and returns a bool
-typedef bool (*PmEveryFunctionInt)(size_t index, int item);
 
 // Returns true if all of the items satisfy the predicate, false otherwise
 bool pm_arr_every_int(int arr[], size_t arr_length, PmEveryFunctionInt predicate);
 
-// A function that receives the current index and item, and returns a bool
-typedef bool (*PmFilterFunctionInt)(size_t index, int item);
-
 // Returns an array which is a subset of the specified array, where all of the items satisfy the predicate
 int* pm_arr_filter_int(int arr[], size_t arr_length, PmFilterFunctionInt predicate);
-
-// A function that receives the current index and item, and returns a bool
-typedef bool (*PmFindFunctionInt)(size_t index, int item);
 
 // Returns the first item that satisfies the predicate
 int pm_arr_find_int(int arr[], size_t arr_length, PmFindFunctionInt predicate); // TODO Return a Maybe?
 
 // Returns the first item that satisfies the predicate
 int pm_arr_find_index_int(int arr[], size_t arr_length, PmFindFunctionInt predicate); // TODO Return a Maybe?
-
-// A function that receives the current index and item, and returns a new value
-typedef void* (*PmMapFunction)(size_t index, void* item);
-
-// Applies a map function to every element and returns them in a new array
-void* pm_arr_map(void* arr, size_t item_size, size_t arr_length, PmMapFunction func);
-
-// A function that receives the current index and item, and returns a new value
-typedef void* (*PmMapFunctionInt)(size_t index, int item); // TODO Change void* to int?
 
 // Applies a map function to every element and returns them in a new array
 int* pm_arr_map_int(int arr[], size_t arr_length, PmMapFunctionInt func);
@@ -74,8 +70,24 @@ int* pm_arr_skip_int(int arr[], size_t arr_length, size_t offset);
 // Takes the specified count of items and returns them in a new array
 int* pm_arr_take_int(int arr[], size_t arr_length, size_t count);
 
+char* pm_arr_to_string(int* arr, size_t size);
+
+#pragma endregion
+
+#pragma region char*
+
 char* pm_arr_tail_str(char** arr);
 
-char* pm_arr_to_string(int* arr, size_t size);
+#pragma endregion
+
+#pragma region void*
+
+// A function that receives the current index and item, and returns a new value
+typedef void* (*PmMapFunction)(size_t index, void* item);
+
+// Applies a map function to every element and returns them in a new array
+void* pm_arr_map(void* arr, size_t item_size, size_t arr_length, PmMapFunction func);
+
+#pragma endregion
 
 #endif // PM_ARRAYS_H
