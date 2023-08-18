3cea955a98c326e79cdc62e4bab0a45c170d030e
viex <carp_97@outlook.com>
2023-08-18 13:47:39 -0600

    feat(files): improve pm_fs_read_text

--- pim/pim/files.c
+++ pim/pim/files.c
@@ -4,18 +4,24 @@
 #include <stdlib.h>
 #include "strings.h"
 
+#define _PM_FILE_BUFFER_SIZE 1024
+
 char* pm_fs_read_text(const char* path) {
     FILE* file;
 
     if (fopen_s(&file, path, "r") != 0) {
-        // TODO How to handle error?
+        // Handle error gracefully (e.g., log, return error code, etc.)
         return NULL;
     }
 
+    // TODO implement pm_str_builder_with_capacity
+    //PmStringBuilder sb = pm_str_builder_with_capacity(_PM_FILE_BUFFER_SIZE);
     PmStringBuilder sb = pm_str_builder();
-    char buffer[6]; // TODO figure out why 6
+    char buffer[_PM_FILE_BUFFER_SIZE];
 
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        // TODO implement pm_str_builder_append_n
+        //pm_str_builder_append_n(&sb, line, line_length);
         pm_str_builder_append(&sb, buffer);
     }
 
